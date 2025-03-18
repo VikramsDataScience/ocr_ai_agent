1fb29e1284f0abee7d5ba797ff024972bbd73359
Vikram Pande <vikrampande28@gmail.com>
2025-03-18 14:42:33 +1100

    Developed agent.py module, but there are some access issues with HuggingFace

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Relative imports
 from .config import FilePaths
@@ -9,4 +11,8 @@ __version__ = "0.01"
 # Load storage paths from config.py
 file_paths = FilePaths()
 api_keys_path = Path(file_paths.api_keys)
-ocr_output_path = Path(file_paths.ocr_output)
+data_path = Path(file_paths.data_path)
+
+# Load environment file containing API keys, and grab the Mistral API key
+load_dotenv(api_keys_path)
+mistral_api_key = os.getenv("MISTRAL_API_KEY", "")
--- src/agent.py
+++ src/agent.py
@@ -1,18 +1,42 @@
-from smolagents import CodeAgent, HfApiModel, DuckDuckGoSearchTool, load_tool, tool
+from smolagents import CodeAgent, HfApiModel, tool
 from smolagents.default_tools import FinalAnswerTool
 
+import gradio as gr
+
 # Relative imports
-from .tools import ocr_tool
+from .tools import mistral_ocr_tool
 
+@tool
+def call_ocr_tool(document_url: str) -> str:
+    """A tool that performs OCR on a document using Mistral's API.
 
+    Args:
+        document_url: The URL of the document to be processed. 
+        Must be a publicly accessible URL pointing to a PDF document,
+        and the user must provide this URL to the agent."""
+
+    return mistral_ocr_tool(document_url)
 
 
 # Define the agent
-# final_answer_tool = FinalAnswerTool()
+final_answer_tool = FinalAnswerTool()
 # model = HfApiModel(
 #     model_id="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
+model = HfApiModel(max_tokens=2096,
+    temperature=0.5,
+    model_id='Qwen/Qwen2.5-Coder-32B-Instruct',
+    custom_role_conversions=None,
+)
 
-
-
-# search_tool("What is the capital of France?")
-# ocr_tool("https://arxiv.org/pdf/2501.12948")
+agent = CodeAgent(
+    model=model,
+    tools=[final_answer_tool,
+           call_ocr_tool],
+        max_steps=6,
+    verbosity_level=1,
+    grammar=None,
+    planning_interval=None,
+    name=None,
+    description=None,
+)
+agent.run("Perform a summary of the document at this URL: https://arxiv.org/pdf/2501.12948")
