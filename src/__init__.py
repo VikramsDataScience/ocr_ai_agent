73326ed7e9c7a0c27bd7ca8a27185501f6239e01
Vikram Pande <vikrampande28@gmail.com>
2025-03-24 12:19:44 +1100

    Agent is now working to answer questions on any PDF document. Modified FilePaths class to export from __init__.py module

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,10 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-# Relative imports
-from .config import FilePaths
+class FilePaths:
+    def __init__(self):
+        self.api_keys = "C:/ocr_ai_agent/api_keys.env"
+        self.data_path = "C:/ocr_ai_agent/data/"
 
 # Model version
 __version__ = "0.01"
