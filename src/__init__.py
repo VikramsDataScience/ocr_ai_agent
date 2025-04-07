5b17f5d5e13b0358a5cc2e55b22ade4e883cba93
Vikram Pande <vikrampande28@gmail.com>
2025-04-07 13:46:08 +1000

    Added new tool to agent that can read CSV/XLSX files. Also trying to use LLM on local machine, but having difficulties with low spec GPU

--- src/__init__.py
+++ src/__init__.py
@@ -28,5 +28,29 @@ langfust_host = os.getenv("LANGFUSE_HOST")
 
 # Define LLMs to use
 qwen_32B = "Qwen/Qwen2.5-Coder-32B-Instruct"
-llama_3B = "meta-llama/Llama-3.2-3B-Instruct"
-llama_1B = "meta-llama/Llama-3.2-1B-Instruct"
+qwen_1halfB = "Qwen/Qwen2.5-Coder-1.5B-Instruct"
+qwen_7BQuant = "Qwen/Qwen2.5-Coder-7B-Instruct-GPTQ-Int4"
+qwen_1half5BQuant = "Qwen/Qwen2.5-Coder-1.5B-Instruct-AWQ"
+
+######### PROMPT TEMPLATES #########
+# If required, call the following prompt templates ('system_prompt' arg) using the PromptTemplates() class from smolagents
+phi_4_3point8B = "microsoft/Phi-4-mini-instruct"
+phi_4_3point8B_prompt_template = """
+<|system|>You are a helpful assistant with some tools.
+<|tool|>[{"name": "call_ocr_tool", 
+"description": "A tool that performs OCR on a document using Mistral's API.", 
+"parameters": {"document_url": 
+{"description": "The URL of the document to be processed. 
+        Must be a publicly accessible URL pointing to a PDF document,
+        and the user must provide this URL to the agent.", 
+"type": "str"}}},
+{"name": "call_ddgs_search_tool",
+"description": "A tool that performs a DuckDuckGo search. Use this tool if you require
+    additional information from the web that is not provided by the user. Please
+    use this tool sparingly to avoid API limits.",
+"parameters": {"query":
+{"description": "The search query to be run.",
+"type": "str"}}}
+]
+<|/tool|><|end|>
+<|user|>{prompt}<|end|><|assistant|>"""
--- src/agent.py
+++ src/agent.py
@@ -1,11 +1,13 @@
-from typing import Dict
+from typing import Dict, Optional
+from pathlib import Path
 
-from smolagents import CodeAgent, tool
+from smolagents import CodeAgent, tool, PromptTemplates
 from smolagents.default_tools import FinalAnswerTool
+import pandas as pd
 
 # Relative imports
-from .tools import mistral_ocr_tool, duckduckgo_search_tool
-from . import llama_1B
+from .tools import mistral_ocr_tool, duckduckgo_search_tool, read_csv_xlsx
+from . import qwen_7BQuant
 from .hf_functions import localTransformersModel
 
 
@@ -32,14 +34,29 @@ def call_ddgs_search_tool(query: str) -> Dict[str, list[dict[str, any]]]:
 
     return duckduckgo_search_tool(query)
 
+@tool
+def call_read_csv_xlsx_tool(file_path: Path, sheet_name: Optional[str] = None) -> pd.DataFrame:
+    """A tool that reads a CSV or XLSX file from a user provided file path 
+    and returns a Pandas DataFrame for analysis, or any task that the user 
+    asks the agent to perform on the returned DataFrame.
+    
+    Args:
+        file_path: The path to the CSV or XLSX file to be read.
+        sheet_name: If the _file_path_ suffix is an XLSX, the user needs to provide
+        name of the sheet to read from the XLSX file."""
+
+    return read_csv_xlsx(file_path, sheet_name)
+
 
 # Define and run the agent
 final_answer_tool = FinalAnswerTool()
+model = localTransformersModel(qwen_7BQuant)
 
-model = localTransformersModel(llama_1B)
+prompt = "Please list 3 key insights you are able to glean by conducting a search on the Deepseek R1 technical paper. Additionally, see if you can find the technical paper itself on arxiv. If so, please list the URL of the paper."
 
 agent = CodeAgent(
     model=model,
+    # prompt_templates=messages,
     tools=[final_answer_tool,
            call_ocr_tool,
            call_ddgs_search_tool],
@@ -47,8 +64,8 @@ agent = CodeAgent(
     verbosity_level=1,
     grammar=None,
     planning_interval=None,
-    name=None,
-    description=None,
+    name="Research Assistant",
+    description="An AI Agent that can search the web, perform OCR on documents, read CSV/XLSX files, and answer questions based on the information it finds.",
 )
-agent.run("Please list 3 key insights you are able to glean by conducting a search on the Deepseek R1 technical paper. Additionally, see if you can find the technical paper itself on arxiv. If so, please list the URL of the paper.")
+agent.run(prompt)
 # agent.run("Perform a summary of the document at this URL: https://arxiv.org/pdf/2501.12948")
